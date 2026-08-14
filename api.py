7e393b66d2be0071bb1cd7d0c184602444b41cf4
azizi23507 <abdul.azizi@stud.th-deg.de>
2026-08-14 23:39:54 +0200

    fast api with raw sql

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2.errors import UniqueViolation
 from fastapi import FastAPI
 from starlette import status
 conn = psycopg2.connect(
@@ -8,10 +9,12 @@ conn = psycopg2.connect(
     user="postgres",
     password="shabir123"
 )
-cursor = conn.cursor()
+from psycopg2.extras import RealDictCursor
+cursor = conn.cursor(cursor_factory=RealDictCursor)
+app = FastAPI()
 
-get_app = FastAPI()
-@get_app.get("/customer/{customer_id}")
+# endpoint with only path parameter
+@app.get("/customer/{customer_id}")
 def get_customer(customer_id: int):
     cursor.execute("SELECT * FROM customer WHERE id = %s", (customer_id,))
 
@@ -30,55 +33,67 @@ class Customer(BaseModel):
     zip: str
 
 
-post_app = FastAPI()
-@post_app.post("/insert-customer", status_code=status.HTTP_201_CREATED)
+
+# end point with body
+@app.post("/insert-customer", status_code=status.HTTP_201_CREATED)
 def post_customer(customer: Customer):
     try:
-        cursor.execute("INSERT INTO customer (id, name, address, city, state, zip) VALUES"
-                       "(%s, %s, %s, %s, %s, %s)",
-                       (
-                           customer.id,
-                           customer.name,
-                           customer.address,
-                           customer.city,
-                           customer.state,
-                           customer.zip
-                       ))
+        cursor.execute(
+            """INSERT INTO customer
+               (id, name, address, city, state, zip)
+               VALUES (%s, %s, %s, %s, %s, %s)""",
+            (
+                customer.id,
+                customer.name,
+                customer.address,
+                customer.city,
+                customer.state,
+                customer.zip
+            )
+        )
         conn.commit()
         return {"message": "Customer created"}
-    except Exception:
+
+    except UniqueViolation:
         conn.rollback()
-        raise
+        return {"message": "Customer with this ID already exists"}
+
 
 conn1 = psycopg2.connect(
     host="localhost",
     port=5432,
-    database="northwind",
+    database="world",
     user="postgres",
     password="shabir123"
 )
-cursor1 = conn.cursor()
-
-app_countries = FastAPI()
 
 
-@app_countries.get("/countries")
+
+#  endpoint wth only query parameters
+cursor1 = conn1.cursor()
+@app.get("/countries")
 def get_countries(country_population: int, continent: str):
     cursor1.execute("SELECT name, population, continent FROM country WHERE population <= %s AND continent = %s",
                     (country_population, continent))
     return cursor1.fetchall()
 
 
+conn2 = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="northwind",
+    user="postgres",
+    password="shabir123"
+)
+cursor2 = conn2.cursor()
+
 from datetime import date
-
-app_order = FastAPI()
-
-
-@app_order.get("/customer/{customer_id}")
+# endpoints with path parameter and query parameter
+@app.get("/customer/{customer_id}")
 def get_customer_order(customer_id: str, required_date: date):
-    cursor1.execute("SELECT customer_id, ship_name FROM orders WHERE customer_id = %s AND required_date = %s",
+    cursor2.execute("SELECT customer_id, ship_name FROM orders WHERE customer_id = %s AND required_date = %s",
                     (customer_id, required_date))
-    return cursor1.fetchall()
+    return cursor2.fetchall()
 
 
 
